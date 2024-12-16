a2247575c962aa35734b4a4c8ed55da4dce10038
Yegor Cheprasov <yegorcheprasov@gmail.com>
2024-12-16 14:41:03 +0300

    Update some ui in converter

--- lib/features/converter/presentation/view/converter_screen.dart
+++ lib/features/converter/presentation/view/converter_screen.dart
@@ -2,6 +2,8 @@ import 'package:auto_route/auto_route.dart';
 import 'package:crypto_currency/common/presentation/utils/context_ext.dart';
 import 'package:crypto_currency/features/converter/domain/bloc/converter_bloc.dart';
 import 'package:crypto_currency/features/converter/domain/entities/keyboard_type.dart';
+import 'package:crypto_currency/features/converter/presentation/widgets/converter_info.dart';
+import 'package:crypto_currency/features/converter/presentation/widgets/converter_keyboard.dart';
 import 'package:crypto_currency/features/converter/presentation/widgets/keyboard_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -27,54 +29,14 @@ class _ConverterScreenState extends State<ConverterScreen> {
         appBar: AppBar(
           title: Text(context.lang.converter),
         ),
-        body: SafeArea(
+        body: const SafeArea(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
+            padding: EdgeInsets.symmetric(horizontal: 20),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const Column(
-                  children: [
-                    Row(
-                      children: [Text("data")],
-                    )
-                  ],
-                ),
-                LayoutGrid(
-                  columnSizes: [100.px, auto, auto],
-                  rowSizes: const [
-                    auto,
-                    auto,
-                    auto,
-                    auto,
-                  ],
-                  areas: '''
-    one two  three
-    four    five six
-    seven    eight nine
-    dot zero  remove
-  ''',
-                  columnGap: 12,
-                  rowGap: 12,
-                  children: [
-                    KeyboardButton(type: KeyboardType.one).inGridArea('one'),
-                    KeyboardButton(type: KeyboardType.two).inGridArea('two'),
-                    KeyboardButton(type: KeyboardType.three)
-                        .inGridArea('three'),
-                    KeyboardButton(type: KeyboardType.four).inGridArea('four'),
-                    KeyboardButton(type: KeyboardType.five).inGridArea('five'),
-                    KeyboardButton(type: KeyboardType.six).inGridArea('six'),
-                    KeyboardButton(type: KeyboardType.seven)
-                        .inGridArea('seven'),
-                    KeyboardButton(type: KeyboardType.eight)
-                        .inGridArea('eight'),
-                    KeyboardButton(type: KeyboardType.nine).inGridArea('nine'),
-                    KeyboardButton(type: KeyboardType.dot).inGridArea('dot'),
-                    KeyboardButton(type: KeyboardType.zero).inGridArea('zero'),
-                    KeyboardButton(type: KeyboardType.remove)
-                        .inGridArea('remove')
-                  ],
-                ),
+                ConverterInfo(),
+                ConverterKeyboard(),
               ],
             ),
           ),
---
+++ lib/features/converter/presentation/widgets/converter_info.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ConverterInfo extends StatelessWidget {
+  const ConverterInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) => Column();
+}
